94c9ce1dce47bcb895b3ae09a2eb47a5df481e8f
Abeauty <pustovoytenko03@bk.ru>
2023-06-12 13:32:25 +0300

    готово

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,7 +6,8 @@ const HomePage = () => {
       <div className={'home__header'}>
         <div class={"line"}></div>
         <div className={'medium-text'}>A'beauty</div>
-        <div className={'medium-text-text'}>Макияж — это зеркало души. Это особенный вид 
+        <div className={'medium-text1'}>Макияж — это зеркало души. 
+        Это особенный вид 
         женской медитации. Время, проведенное
          наедине с собою и своими мыслями. Момент, 
          когда мягкие взмахи кистью буквально 
@@ -14,8 +15,8 @@ const HomePage = () => {
         <button className={'but'}>Подробнее</button>
         <div className={'kartinki'}>
           <img className={'kartinki01'} src={'/images/1.png'} />
-          <img className={'kartinki2'} src={'/images/2.png'} />
-          <img className={'kartinki3'} src={'/images/3.png'} />
+          <img className={'kartinki02'} src={'/images/2.png'} />
+          <img className={'kartinki03'} src={'/images/3.png'} />
           <img className={'kartinki04'} src={'/images/4.png'} />
         </div>
         <div>
@@ -23,11 +24,11 @@ const HomePage = () => {
             Подпишитесь на наш аккаунт @A'beauty и узнавайте
             о новиках и акциях первыми
           </div>
-          <div className="Best">Продукты, завоевавшие любовь наших клиентов</div>
+          <div className="Love">Продукты, завоевавшие любовь наших клиентов</div>
           <img className={'kartinki11'} src={'/images/p_1.png'}></img>
           <img className={'kartinki12'} src={'/images/p_2.png'}></img>
           <img className={'kartinki13'} src={'/images/p_3.png'}></img>
-         <div className="Best1">Косметика – философия женщин!</div>
+         <div className="Love1">Косметика – философия женщин!</div>
         </div>
         <button className={'but01'}>Подробнее</button>
         <div>
@@ -47,8 +48,8 @@ const HomePage = () => {
           <img className={'g3'} src={'/images/g2.png'}></img>
           <img className={'g4'} src={'/images/g4.png'}></img>
         </div>
-        <div className={'contact'}>
-          <img className={'Cont'} src={'/images/38.png'}></img>
+        <div className={'info'}>
+          <img className={'info1'} src={'/images/38.png'}></img>
           
         </div>
         <h1 class="otstup0">Неважно</h1>    
